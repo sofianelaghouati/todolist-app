eb0932d73fe8a5271c11f7192ab54631d2fccd5e
Laghouati Mohamed Sofiane <sofianelaghouatipro@gmail.com>
2025-01-18 15:14:18 +0100

    Adding update button with func now

--- src/Todo.js
+++ src/Todo.js
@@ -1,23 +1,66 @@
 import React, { Component } from 'react'
 
 
-class Todo extends Component{
-    constructor(props){
+class Todo extends Component {
+    constructor(props) {
         super(props)
+        this.state = {
+            isEditing: false,
+            task: this.props.task
+        }
         this.handleRemove = this.handleRemove.bind(this)
+        this.toggleForm = this.toggleForm.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleRemove(){
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    handleRemove() {
         this.props.removeTodo(this.props.id)
     }
-    render(){
-        return(
-            <div>
-                <li>{this.props.task}</li>
-                <button>Edit</button>
-                <button onClick={this.handleRemove}>X</button>
-            </div>
-        )
+
+    toggleForm(){
+        this.setState({
+            isEditing: !this.state.isEditing
+        })
+    }
+
+    handleUpdate(evt){
+        evt.preventDefault()
+        
+    }
+
+    render() {
+        let result;
+        if (this.state.isEditing) {
+            result = (
+                <div>
+                    <form onSubmit={this.handleUpdate}>
+                        <input
+                            type='text'
+                            value={this.state.task}
+                            name="task"
+                            onChange={this.handleChange}
+                        />
+                        <button>Save</button>
+                    </form>
+                </div>
+            )
+        }
+        else {
+            result = (
+                <div>
+                    <li>{this.props.task}</li>
+                    <button onClick={this.toggleForm}>Edit</button>
+                    <button onClick={this.handleRemove}>X</button>
+                </div>
+            )
+        }
+        return result
     }
 }
 
